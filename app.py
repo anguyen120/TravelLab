81abd60c4db252fb1a6e1a9329a49d4affb9e6ca
anguyen120 <anguyen120@uh.edu>
2020-04-02 12:44:42 -0500

    Add an html file to test api keys

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Load API Keys
+unsplash_api_key = os.getenv('UNSPLASH_API_KEY')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    print(unsplash_api_key)
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("index.html", value=unsplash_api_key)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
