29e203c4873525cec6254c271c37b73ad5ae30ed
anguyen120 <anguyen120@uh.edu>
2020-04-02 19:27:54 -0500

    Integrate API keys

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-import os
 from flask import Flask, render_template
+import settings
 
 app = Flask(__name__)
 
-# Load API Keys
-unsplash_api_key = os.getenv('UNSPLASH_API_KEY')
 
 @app.route('/')
 def index():
-    print(unsplash_api_key)
-    # We will just display our mailgun secret key, nothing more.
-    return render_template("index.html", value=unsplash_api_key)
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
