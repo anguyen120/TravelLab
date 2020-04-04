9902804c6a783d0ba1f1c331b32ad6d789523e59
anguyen120 <anguyen120@uh.edu>
2020-04-03 19:09:25 -0500

    Set up prototype of website with placeholder images for demo

--- app.py
+++ app.py
@@ -6,7 +6,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    return render_template('results.html')
+
+
+@app.route('/attractions')
+def attractions():
+    return render_template('attractions.html')
+
+
+@app.route('/flights')
+def flights():
+    return render_template('flights.html')
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+
+@app.route('/hotels')
+def hotels():
+    return render_template('hotels.html')
 
 
 if __name__ == '__main__':
