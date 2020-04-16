1e55bb574ccdd93fb5f90233ac18eff0e8d9574e
anguyen120 <anguyen120@uh.edu>
2020-04-16 06:46:14 -0500

    Update website format for user study to be presentable. Integrated flight component but isn't fully functional.

--- static/js/attractions.js
+++ static/js/attractions.js
@@ -1,31 +1,12 @@
 (function () {
-    console.log(attractions);
-    const app = document.getElementById('attr_results');
-    const container = document.createElement('div');
-    container.setAttribute('class', 'container');
-
     attractions.data.forEach(function (attraction) {
-        //const card = document.createElement('div');
-        //card.setAttribute('class','card');
-        var h1 = document.createElement('h2');
-        h1.textContent = attraction["name"];
-        var addr = document.createElement('p');
-        addr.textContent = attraction["address"];
-        var description = document.createElement('p');
-        description.textContent = attraction["description"];
-        var rating = document.createElement('p');
-        rating.textContent = attraction["rating"] + " / 5.0";
-        var a = document.createElement('a');
-        var link = document.createTextNode("View on TripAdvisor");
-        a.appendChild(link);
-        a.title = "View on TripAdvisor";
-        a.href = attraction["web_url"];
-        app.appendChild(container);
-        container.appendChild(h1);
-        container.appendChild(addr);
-        container.appendChild(description)
-        container.appendChild(rating);
-        container.appendChild(a);
+        document.getElementById('attractions').innerHTML += `<h2 style="margin-bottom: 0px;padding-bottom: 2%;">${attraction["name"]}</h2>` +
+            `<span class="image main" style="margin-bottom: 0;padding-bottom: 2%;width:inherit;overflow: hidden;"><img src="${attraction["photo"]["images"]["original"]["url"]}" alt="" style="margin:-21.875% 0;"></span>` +
+            `<p style="margin-bottom: 0px;"><b>${attraction["rating"]} / 5.0</b></p>` +
+            `<p style="margin-bottom: 0px;"><b>${attraction["address"]}</b></p>` +
+            `<p style="margin-bottom: 0px;padding-bottom: 1%;">${attraction["description"]}</p>` +
+            `<p style="margin-bottom: 0px;padding-bottom: 5%;"><a href="${attraction["web_url"]}">View on TripAdvisor</a></p>`
+        ;
 
         return;
     });
---
+++ static/js/results.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    attractions.data.forEach(function (attraction) {
+        document.getElementById('attractions').innerHTML += `<h4 style="margin-bottom: 0px;padding-bottom: 2%;"><a href="${attraction["web_url"]}">${attraction["name"]}</a></h4>` +
+            `<p style="margin-bottom: 0px;">${attraction["rating"]} / 5.0</p>` +
+            `<p style="margin-bottom: 0px;padding-bottom: 5%;">${attraction["address"]}</p>`
+        ;
+
+        return;
+    });
+
+    photos["results"].forEach(function (photo) {
+        document.getElementById("gallery").innerHTML += `<article class="style1">` +
+            `<span class="image">` +
+            `<img src="${photo["urls"]["regular"]}" alt="${photo["urls"]["full"]}" />` +
+            `</span>` +
+            `<div class="content">` +
+            `<p>Photo by <a href="https://unsplash.com/@${photo["user"]["username"]}?utm_source=travellab&utm_medium=referral">${photo["user"]["name"]}</a> on <a href="https://unsplash.com/?utm_source=travellab&utm_medium=referral">Unsplash</a></p>` +
+            `</div>` +
+            `</article>`
+        ;
+    });
+
+    hotels.data.forEach(function (hotel) {
+        document.getElementById('hotels').innerHTML += `<h4 style="margin-bottom: 0px;padding-bottom: 1%;">${hotel["name"]}</h4>` +
+            `<p style="margin-bottom: 0px;">${hotel["rating"]} / 5.0</p>`
+        ;
+
+        if ((hotel["business_listings"]["desktop_contacts"]).length >= 1) {
+            document.getElementById('hotels').innerHTML += `<p style="margin-bottom: 0px;padding-bottom: 5%;"><a href="${hotel["business_listings"]["desktop_contacts"][0]["value"]}">View Booking Information</a></p>`;
+        } else {
+            document.getElementById('hotels').innerHTML += `<p style="margin-bottom: 0px;padding-bottom: 5%;">Booking Information Not Available</p>`;
+        }
+
+        return;
+    });
+};
